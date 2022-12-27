26dd88025ec2fe19107f5aba2ff149e654d1ac2a
SK927 <m.lefebv@gmail.com>
2022-12-27 15:38:55 +0100

    Edit EN localization
    + correct FR typos + add new strings support

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -2,11 +2,11 @@ module CompetitionsHelper
   def class_and_status_from_person(sub_by_id, sub_by_name, person)
     if @subscribers_by_id.include?(person["wcaId"]) ||
        @subscribers_by_name.include?(I18n.transliterate(person["name"].downcase))
-      ["success", "Pris en charge par l'AFS"]
+      ["success", I18n.t("competitions.registrations.subscribed")]
     elsif person["wcaId"]
-      ["danger", "Doit payer"]
+      ["danger", I18n.t("competitions.registrations.must_pay")]
     else
-      ["warning", link_to("Vérifier s'il est nouveau compétiteur", "#", class: "link-check", data: { id: person["registration"]["wcaRegistrationId"] })]
+      ["warning", link_to(I18n.t("competitions.registrations.check_if_new"), "#", class: "link-check", data: { id: person["registration"]["wcaRegistrationId"] })]
     end
   end
 end
